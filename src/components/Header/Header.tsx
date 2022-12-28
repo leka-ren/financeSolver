b32c31b8143452df4807d9d295292d1dbb6a0748
Polina Ren <56325631+leka-ren@users.noreply.github.com>
2022-12-28 17:16:18 +0800

    maked new design for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,17 @@
 import { ComponentType } from "react";
-import style from "./Header.module.scss";
 
-import cx from "classnames";
+import Logo from "../../images/Logo.svg";
+
+import style from "./Header.module.scss";
 
 export const Header: ComponentType = () => {
   return (
     <div className={style.content}>
       <div className={style.template}>
-        <h1 className={style.text}>FinanceSolwer</h1>
+        <img src={Logo} alt={"logo - Finance Solwer"} className={style.logo} />
         <div className={style.authButtons}>
-          <button className={cx(style.text, style.button)}>SignUp</button>
-          <button className={cx(style.text, style.button)}>LogIn</button>
+          <button className={style.button}>LogIn</button>
+          <button className={style.button}>SignUp</button>
         </div>
       </div>
     </div>
