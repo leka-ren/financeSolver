ae8ac473f56e213a0d573b3a02a4b705991483df
Polina Ren <56325631+leka-ren@users.noreply.github.com>
2024-04-01 15:50:35 +0800

    fix header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,10 @@
 import { ComponentType } from "react";
 import style from "./Header.module.scss";
 
-import cx from "classnames";
-
 export const Header: ComponentType = () => {
   return (
     <div className={style.content}>
-      <div className={style.template}>
-        <h1 className={style.text}>FinanceSolwer</h1>
-        <div className={style.authButtons}>
-          <button className={cx(style.text, style.button)}>SignUp</button>
-          <button className={cx(style.text, style.button)}>LogIn</button>
-        </div>
-      </div>
+      <h1 className={style.title}>FinanceSolwer</h1>
     </div>
   );
 };
